b06aff32866ad64c4d9e427344846a98ca28dffd
jvs-alpha <jvsamuel.rufus@gmail.com>
2022-09-15 00:11:50 +0530

    the second part is not working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cheerio = require("cheerio");
 
 const url = "https://getcomics.info/?s=";
 
+
 async function scrape() {
     try {
         const {data} = await axios.get(url);
@@ -11,7 +12,7 @@ async function scrape() {
         const parsed = cheerio.load(data);
         const postList = parsed("div[class=post-header-image] > a");    // This bacically searches for all the div with class as post-head-image and get the a tag from it
         const postUrl = new Array();
-        // console.log(postList);
+        // console.log(postList.__proto__);
         postList.each((idx, el) => {
             // console.log(Object.keys(el));
             let val = parsed(el).attr().href;
@@ -21,7 +22,12 @@ async function scrape() {
         // console.log(postUrl);
         for (let ip in postUrl)
         {
-            console.log(postUrl[ip]);
+            // console.log(typeof postUrl[ip]);
+            const {data2} = await axios.get(postUrl[ip]);
+            console.log(data2);
+            const parsed2 = cheerio.load(data2);
+            // console.log(parsed2);
+            break;
         }
         
 
